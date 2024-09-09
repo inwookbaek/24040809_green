bd6a54a2ec3c0a7ee1cbc7c09b4c72d6077d682c
inwookbaek <iwbaek@gmail.com>
2024-09-09 17:36:28 +0900

    20240909

--- 03.database/16.function.sql
+++ 03.database/16.function.sql
@@ -165,41 +165,248 @@ select ename
   from emp;
 
 
-/* 실습문제 */
+/* 실습문제 */ 
 -- ex01) 부서의 총 급여를 반환하는 함수
--- get_dept_total_salary
+-- get_dept_total_salary(10)
+create or replace function get_dept_total_salary(p_deptno in hr.employees.department_id%type) return number
+is	
+	v_total_salary  number;
+begin
+	select sum(salary) 
+	  into v_total_salary
+	  from hr.employees
+	 where department_id = p_deptno;
+		
+		return nvl(v_total_salary, 0);
+end;
+
+select get_dept_total_salary(20) from dual;
+select get_dept_total_salary(10) from dual;
+
+select deptno, sum(salary)
+	from hr.employees
+ where department_id = 10; -- group by 에러
+
+select department_id
+     , get_dept_total_salary(department_id)
+	from hr.employees
+ where department_id = 10;
+ 
+select * From hr.employees;
 
 -- ex02) 직원 번호가 주어졌을 때 직원의 관리자 이름을 반환하는 함수
 -- get_manager_name
+create or replace function get_manager_name(p_emp_id in hr.employees.employee_id%type) return varchar2
+is	
+	v_mgr_name  hr.employees.first_name%type;
+begin
+	select first_name || '.' || last_name
+	  into v_mgr_name
+	  from hr.employees
+	 where employee_id = (select manager_id 
+	                        from hr.employees 
+												 where employee_id = p_emp_id);
+												 
+	return v_mgr_name;
+exception 
+	when no_data_found then return '해당 사원(' || p_emp_id ||')의 매니저가 없습니다!';
+end;
+
+select get_manager_name(100) from dual;
+select get_manager_name(101) from dual;
+
+select employee_id
+     , first_name || '.' || last_name 사원이름
+		 , get_manager_name(employee_id)  매니저이름
+	from hr.employees
+ where get_manager_name(employee_id) = 'John.Russell';
 
 -- ex03) 급여와 커미션을 기준으로 직원의 커미션 비율을 계산하는 함수
 -- get_commission_pct
+create or replace function get_commission_pct(p_emp_id in hr.employees.employee_id%type) return number
+is
+	v_salary   hr.employees.salary%type;
+	v_comm     hr.employees.commission_pct%type;
+begin
+	select salary, commission_pct
+	  into v_salary, v_comm
+	  from hr.employees
+	 where employee_id = p_emp_id;
+	 
+  if v_comm is null or v_comm = 0 then
+		return 0;
+	else
+		return v_comm * 100;
+	end if;
+			
+exception 
+	when no_data_found then return -1;
+end;
+
+select get_commission_pct(100) from dual;
+select get_commission_pct(150) from dual;
 
 -- ex04) 부서의 직원 수를 반환하는 함수
 -- count_employees_in_dept
+select * From hr.employees;
+create or replace function count_employees_in_dept(p_deptno in hr.employees.department_id%type) return number
+is
+	v_count   number;
+begin
+	select count(*) 
+	  into v_count
+	  from hr.employees
+	 where department_id = p_deptno;
+	 
+	 return nvl(v_count, 0);
+	 
+end;
+
+select count_employees_in_dept(90) from dual;
+
+select department_id
+    , department_name
+	  , count_employees_in_dept(department_id)
+  from hr.departments;
+
 
 -- ex05) 부서에서 가장 높은 급여를 받는 직원의 이름을 반환하는 함수
--- get_highest_paid_emp
+-- get_highest_paid_emp 
+create or replace function get_highest_paid_emp(p_deptno in hr.employees.department_id%type) return varchar2
+is
+	v_emp_name  hr.employees.first_name%type;
+begin
+	select emp_name
+	  into v_emp_name
+	  from (select rownum rn, first_name || '.' || last_name emp_name
+						from hr.employees
+					 where department_id = p_deptno
+					 order by salary desc)
+	 where rn = 1;
+	 
+	 return v_emp_name;
+end;
 
+select department_id
+    , department_name
+	  , get_highest_paid_emp(department_id) 최고급여사원이름
+  from hr.departments;
+	 
 -- ex06) 직원이 관리자인지 확인하는 함수
 -- is_manager
+create or replace function is_manager(p_empno in hr.employees.employee_id%type) return varchar2
+is
+	v_count  number;
+begin
+	select count(*)
+	  into v_count
+	  from hr.employees
+	 where manager_id = p_empno;
+	 
+	 if v_count > 0
+			then return '관리자입니다!!';
+			else return '관리자가 아닙니다!!';
+	 end if;
+end;
+
+select employee_id
+     , first_name || '.' || last_name
+		 , is_manager(employee_id)
+  from hr.employees;
 
 -- ex07) 특정 관리자에게 보고하는 직원의 평균 급여를 구하는 함수
 -- get_avg_salary_by_mgr
+create or replace function get_avg_salary_by_mgr(p_mgrid in hr.employees.employee_id%type) return number
+is
+	v_avg_salary  number;
+begin
+	select avg(salary)
+	  into v_avg_salary
+    from hr.employees
+	 where manager_id = p_mgrid;
+	 
+	 return nvl(v_avg_salary, 0);
+end;
 
--- ex08) 직원의 근속 기간을 계산하는 함수
+select get_avg_salary_by_mgr(101) from dual;
+select get_avg_salary_by_mgr(120) from dual;
+
+
+-- ex08) 직원의 근속 기간을 계산하는 함수(년수)
 -- get_years_of_service
+select * from hr.employees;
+create or replace function get_years_of_service(p_empid in hr.employees.employee_id%type) return number
+is
+	v_hiredate   hr.employees.hire_date%type;
+	v_years      number;
+begin
+	select hire_date 
+	  into v_hiredate
+    from hr.employees
+	 where employee_id = p_empid;
+	 
+	 v_years := floor((sysdate - v_hiredate) / 365);
+	 
+	 return nvl(v_years, 0);
+end;
 
+select employee_id
+     , first_name || '.' || last_name  사원이름
+		 , get_years_of_service(employee_id) || ' 년' 근속년수
+  from hr.employees
+ where employee_id = 100;
+	
 -- ex09) 직원이 속한 부서의 이름을 반환하는 함수
 -- get_dept_name_by_emp
+create or replace function get_dept_name_by_emp(p_empid in hr.employees.employee_id%type) return varchar2
+is
+	v_dept_name   hr.departments.department_name%type;
+begin
+	select dpt.department_name
+	  into v_dept_name
+	  from hr.employees   emp
+		   , hr.departments dpt
+	 where emp.employee_id   = p_empid
+	   and emp.department_id = dpt.department_id;
+
+	return v_dept_name;
+end;
+
+select employee_id
+     , first_name || '.' || last_name  사원이름
+		 , get_dept_name_by_emp(employee_id) 부서이름
+  from hr.employees
+ where employee_id = 150;
+ 
 
 -- ex10) 직원과 관리자 간의 급여 차이를 계산하는 함수
 -- get_salary_difference_with_mgr
+create or replace function get_salary_difference_with_mgr(p_empid in hr.employees.employee_id%type) return number
+is
+	v_emp_sal   hr.employees.salary%type;
+	v_mgr_sal   hr.employees.salary%type;
+	
+begin
+	select salary
+	  into v_emp_sal
+	  from hr.employees
+	 where employee_id = p_empid;
+	 
+	select salary
+	  into v_mgr_sal
+	  from hr.employees
+	 where employee_id = (select manager_id 
+	                       from hr.employees 
+												where employee_id = p_empid);		
+												
+	return v_emp_sal - v_mgr_sal;
 
+exception
+	when no_data_found then return -1; -- 사원 or 관리자를 찾지 못했을 경우
+end get_salary_difference_with_mgr;
 
-
-
-
-
-
-
+select employee_id
+     , first_name || '.' || last_name  사원이름
+		 , get_salary_difference_with_mgr(employee_id) 급여차이
+  from hr.employees
+ where employee_id = 150;
