a1f364f40764f071e9e601ff0fa40f4c919cb9ed
inwookbaek <iwbaek@gmail.com>
2024-09-10 18:12:25 +0900

    20241010

---
+++ 04.java/java01.variables/src/com/lec/ex01_hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lec.ex01_hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World(1)!!!");
+		System.out.println("Hello World(2)!!!");
+		System.out.println("Hello World(3)!!!");
+	}
+
+}
