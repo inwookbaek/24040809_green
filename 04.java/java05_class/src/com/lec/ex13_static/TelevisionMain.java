6315c34b5d0ffff727acb530d0c39425c81971f2
inwookbaek <iwbaek@gmail.com>
2024-09-19 18:21:04 +0900

    20240919

---
+++ 04.java/java05_class/src/com/lec/ex13_static/TelevisionMain.java
@@ -0,0 +1,11 @@
+package com.lec.ex13_static;
+
+public class TelevisionMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("정적초기화블럭을 이용한 정보 = " + Television.info);
+
+	}
+
+}
